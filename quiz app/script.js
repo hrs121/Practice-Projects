4be0acf693a1a4a10ecbe8849266491fc95c74aa
Md Humayun Rashid <92227819+hrs121@users.noreply.github.com>
2023-09-18 03:05:37 +0600

    first commit

---
+++ quiz app/script.js
@@ -0,0 +1,102 @@
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const container=document.getElementById('quiz')
+const question=document.getElementById('question')
+const answer=document.querySelectorAll('.answer')
+const a_text=document.getElementById('a_text')
+const b_text=document.getElementById('b_text')
+const c_text=document.getElementById('c_text')
+const d_text=document.getElementById('d_text')
+const btn=document.getElementById('submit')
+
+let quiz=0;
+let score=0;
+
+let loadQuiz=()=>{
+    deselect()
+   let currentQuiz=quizData[quiz]
+    question.innerHTML=currentQuiz.question
+    a_text.innerHTML=currentQuiz.a
+    b_text.innerHTML=currentQuiz.b
+    c_text.innerHTML=currentQuiz.c
+    d_text.innerHTML=currentQuiz.d
+}
+loadQuiz();
+
+
+
+function deselect(){
+    answer.forEach((answer)=>answer.checked=false)
+}
+
+
+
+function checkAnswer(){
+    let answerCheck;
+
+    answer.forEach((answerE)=>{
+        if(answerE.checked){
+            answerCheck=answerE.id
+        }
+    })
+    return answerCheck
+}
+
+
+
+btn.addEventListener('click',()=>{
+    let ans=checkAnswer();
+
+    if(ans){
+        if(ans===quizData[quiz].correct){
+            score++;
+        }
+    }
+
+    quiz++;
+
+    if(quiz < quizData.length){
+        loadQuiz();
+    }else{
+        container.innerHTML= `
+        <img src='https://w7.pngwing.com/pngs/296/544/png-transparent-multicolored-congratulations-dunottar-school-youtube-competition-s-congratulations-icon-miscellaneous-text-logo.png' height=430px width=606px >
+
+        <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+        <button onclick="location.reload()">Re Submit</button>
+    `
+    }
+})
